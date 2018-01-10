19d87135f4d0c4581d105d34337463069dff6291
zook2005 <zook2005@gmail.com>
2018-01-10 13:05:51 +0100

    refactored Move method by adding the command class

--- code/Tests/MarsTests/MarsRoverTests.cs
+++ code/Tests/MarsTests/MarsRoverTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace MarsTests
 {
@@ -16,26 +17,28 @@ namespace MarsTests
         {
             //Arrange
             Point startingPoint = new Point(0, 0);
-            char startingDirection = 'e';
+            MarsRover.CardinalDirection startingDirection = MarsRover.CardinalDirection.East;
             MarsRover rover = new MarsRover(startingPoint, startingDirection);
             var moves = new[] { 'f' };
 
             Point expectedCoordinates = new Point(1, 0);
+            MarsRover.CardinalDirection expectedStartingDirection = MarsRover.CardinalDirection.East;
 
             //Act
             var roverAtNewCoordinates = rover.Move(moves);
 
             //Assert
             Assert.AreEqual(expectedCoordinates, roverAtNewCoordinates.coordinates);
+            Assert.AreEqual(expectedStartingDirection, roverAtNewCoordinates.direction);
         }
     }
 
     internal class MarsRover
     {
         internal Point coordinates;
-        internal char direction;
+        internal CardinalDirection direction;
 
-        public MarsRover(Point startingPoint, char startingDirection)
+        public MarsRover(Point startingPoint, CardinalDirection startingDirection)
         {
             this.coordinates = startingPoint;
             this.direction = startingDirection;
@@ -43,9 +46,87 @@ namespace MarsTests
 
         internal MarsRover Move(char[] moves)
         {
-            Point coordinates = new Point(1, 0);
-            char direction = 'e';
+            Point coordinates = this.coordinates;
+            CardinalDirection direction = this.direction;
+
+            foreach (char commandChar in moves)
+            {
+                BaseCommand command = CommanfFactory(commandChar);
+                RoverState newState = command.CalcNewState(coordinates, direction);
+
+                direction = newState.direction;
+                coordinates = newState.coordinates;
+            }
+
             return new MarsRover(coordinates, direction);
+        }
+
+        internal enum CardinalDirection
+        {
+            North,
+            East,
+            South,
+            West
+        }
+
+        internal BaseCommand CommanfFactory(char command)
+        {
+            switch (command)
+            {
+                case 'f':
+                    return new MoveForwardCommand();
+            }
+            return null;
+        }
+    }
+
+    internal class RoverState
+    {
+        internal MarsRover.CardinalDirection direction;
+        internal Point coordinates;
+
+        public RoverState(Point coordinates, MarsRover.CardinalDirection direction)
+        {
+            this.coordinates = coordinates;
+            this.direction = direction;
+        }
+
+        public static ReadOnlyDictionary<MarsRover.CardinalDirection, Vector> CardinlDirectionToMoveDictionary = new ReadOnlyDictionary<MarsRover.CardinalDirection, Vector>(new Dictionary<MarsRover.CardinalDirection, Vector>
+        {
+            { MarsRover.CardinalDirection.North,    new Vector(0, 1)    },
+            { MarsRover.CardinalDirection.East,     new Vector(1, 0)    },
+            { MarsRover.CardinalDirection.South,    new Vector(0, -1)   },
+            { MarsRover.CardinalDirection.West,     new Vector(-1, 0)   }
+        });
+    }
+
+    internal class MoveForwardCommand : BaseCommand
+    {
+        protected override MarsRover.CardinalDirection CalcNewDirection(MarsRover.CardinalDirection direction)
+        {
+            MarsRover.CardinalDirection newDirection = direction; //walking forward, direction has not changed
+            return newDirection;
+        }
+    }
+
+    internal abstract class BaseCommand
+    {
+        public RoverState CalcNewState(Point coordinates, MarsRover.CardinalDirection direction)
+        {
+            var newDirectaion = CalcNewDirection(direction);
+            Vector move = CalcNextMove(coordinates, newDirectaion);
+
+            var newCoordinates = Point.Add(coordinates, move);
+
+            return new RoverState(newCoordinates, newDirectaion);
+        }
+
+        protected abstract MarsRover.CardinalDirection CalcNewDirection(MarsRover.CardinalDirection direction);
+
+        protected Vector CalcNextMove(Point coordinates, MarsRover.CardinalDirection newDirectaion)
+        {
+            Vector newCoordinates = RoverState.CardinlDirectionToMoveDictionary[newDirectaion];
+            return newCoordinates;
         }
     }
 }
